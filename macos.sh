1b282a727c320645777ae1362a15cc0b001c48e9
Dylan Randle <dylanrandle@rogers.com>
2024-01-25 18:37:32 -0500

    add awscli

--- macos.sh
+++ macos.sh
@@ -2,7 +2,7 @@
 echo "Running installation..."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install htop nvm mas
+brew install htop nvm mas awscli
 brew install --cask iterm2 visual-studio-code slack brave-browser spotify docker miniconda adobe-acrobat-reader android-studio
 mas install 497799835 # Xcode
 
